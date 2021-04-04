43570f6a90f742674d5c02bfdffa92d62912f13c
Pagucada <pagucada@gmail.com>
2021-04-04 17:01:51 -0300

    3

---
+++ validateParenthesisFunction.js
@@ -0,0 +1,51 @@
+function areValidParenthesis(string_1) 
+	{
+		a = '(';
+		a1 = ')';
+		b = '[';
+		b1 = ']';
+		c = '{';
+		c1 = '}';
+		abierto = 0;
+		array_1 = string_1.split('');
+
+        if (array_1[0] == a1 || array_1[0] == b1 || array_1[0] == c1)
+        {
+            return false
+        }
+       
+		for (i = 0; i < array_1.length; i++)
+		{
+			
+			if (array_1[i] == a)
+			{
+				if (array_1[i+1] == b1 || array_1[i+1] == c1)
+				{
+					return false
+				}
+			}
+			if (array_1[i] == b)
+			{
+				if (array_1[i+1] == c1 || array_1[i+1] == a1)
+				{
+					return false
+				}
+			}
+			if (array_1[i] == c)
+			{
+				if (array_1[i+1] == b1 || array_1[i+1] == a1)
+				{
+					return false
+				}
+			}
+            
+		}	
+        if (array_1.length%2 === 0)
+		{
+			return true
+		}
+        else if (array_1.length%2 != 0)
+		{
+            return false
+        }
+    }
